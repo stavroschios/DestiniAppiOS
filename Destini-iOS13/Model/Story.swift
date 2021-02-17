9de83117a94ee5de9eb7ccfe3bf9ad6255e2ed63
Stavros <stavroschios@gmail.com>
2021-02-17 10:17:15 -0500

    first commit

---
+++ Destini-iOS13/Model/Story.swift
@@ -0,0 +1,17 @@
+//
+//  Sotry.swift
+//  Destini-iOS13
+//
+//  Created by Stavros Pachoundakis on 17/02/2021.
+//  Copyright © 2021. All rights reserved.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
